1cfa2a74aaa82598691a9a563f12d170d74c657c
antonio vazquez <antonio.vazquez@axity.com>
2019-07-17 12:00:21 -0500

    se agregan archivos de imnicio

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MessagingService } from './shared/messaging.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-push-notification';
+  message: any = null;
+  constructor(private messagingService: MessagingService) { }
+
+  ngOnInit() {
+    const userId = 'user001';
+    this.messagingService.requestPermission(userId);
+    this.messagingService.receiveMessage();
+    this.message = this.messagingService.currentMessage;
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBB6PgAgEI8EMKRCULnxuJ9BuxAS0eCRmA',
+    authDomain: 'spider-8861e.firebaseapp.com',
+    databaseURL: 'https://spider-8861e.firebaseio.com',
+    projectId: 'spider-8861e',
+    storageBucket: 'spider-8861e.appspot.com',
+    messagingSenderId: '798857087226',
+    appId: '1:798857087226:web:d110548c55a76ca4'
+  }
+};
